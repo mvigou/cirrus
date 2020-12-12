17b45b1b2a87848c028fe02681bac002755a8e83
Julien Wilhelm <contact@awebsome.fr>
2020-12-12 16:42:43 +0100

    Ajust the process when a zip is builded for get success in all cases.

--- async-build-zip.php
+++ async-build-zip.php
@@ -10,12 +10,24 @@ require_once('./common.php');
 
 if(isset($_GET['dir']) && inScopeDirectory($_GET['dir'])) {
 
+	// Corresponding to the parent directory name.
 	$zipName = array_slice(explode('/', $_GET['dir']), -1)[0];
 
+	// Settings.
 	$zip = new ZipArchive();
 	$zip->open(TEMP_DIR_PATH . '/' . $zipName . '.zip', ZipArchive::CREATE);
-	buildZip($zip, $_GET['dir']);
+
+	// Building.
+	buildZip($zip, $zipName, $_GET['dir']);
+	
+	// If the selected directory was empty, add the directory itself to the zip.
+	if($zip->count() === 0) {
+		$zip->addEmptyDir($zipName);
+	}
+	
 	$zip->close();
+
+	// Return the complete path to the zip as an AJAX response.
 	echo TEMP_DIR_PATH . '/' . $zipName . '.zip';
 
 }
@@ -24,20 +36,23 @@ else {
 	echo 'failure';
 }
 
-function buildZip($zip, $dirPath) {
-
-	// Browse selected directory. 
+function buildZip($zip, $zipName, $dirPath) {
+	
 	foreach(array_diff(scandir($dirPath), array('..', '.')) as $dirEntry) {
-		
-		// File ? Add to zip.
-		if(is_file($dirPath . '/' . $dirEntry)) {
-			$zip->addFile($dirPath . '/' . $dirEntry);
-		}
-		// Folder ? Recursive call.
-		else {
-			buildZip($zip, $dirPath . '/' . $dirEntry);
-		}
 
+		$origPath = $dirPath . '/' . $dirEntry;
+		$destPath = substr($dirPath, strpos($dirPath, $zipName)) . '/' . $dirEntry;
+
+		// File ? Add to zip.
+		if(is_file($origPath)) {
+			$zip->addFile($origPath, $destPath);
+		}
+		// Folder ? Create it, then recursive call of this function.
+		else {
+			$zip->addEmptyDir($destPath);
+			buildZip($zip, $zipName, $origPath);
+		}
+		
 	}
 	
 }
