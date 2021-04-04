8300e7c2b00ee471a13448dd9b7669f6dbefeb86
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-04-04 22:27:22 +0200

    Add condition to prevent to rename a parent folder.

--- app/client/scripts/func-ui.js
+++ app/client/scripts/func-ui.js
@@ -216,39 +216,51 @@
 					attributes: { 
 						class: 'bwr__item'
 					},
-					children: [
-						{ // Replace the next sibling in normal mode.
-							type: 'a',
-							attributes: { 
-								class: 'bwr__item__a non-editable',
-								title: lab.bt.open 
-							},
-							events: [
-								{
-									type: 'click',
-									function: () => {
-										if(item.type === 'file') {
-											accessFile(item.path);
-										}
-										else {
-											browseDirectory(item.path);
-										}
-									}
-								},
-							],
-							children : [
-								{
-									type: 'p',
-									attributes: {
-										class: 'bwr__item__title'
-									},
-									html: item.type === 'file' ? 
-										`<svg viewBox="3 3 18 18"><path d="m15.157 3h-8.5207c-0.90374 0-1.6364 0.73262-1.6364 1.6364v14.727c0 0.9038 0.73262 1.6364 1.6364 1.6364h11.455c0.9037 0 1.6364-0.7326 1.6364-1.6364v-11.793zm-0.3389 4.9091v-2.9338l2.9338 2.9338z" /></svg>${item.label}` :
-										`<svg viewBox="3 3 18 18"><path d="M21 8.27273C21 7.36899 20.2674 6.63636 19.3636 6.63636H12.0015C12.0343 6.63619 12.0239 6.6235 11.9519 6.53598C11.9342 6.51449 11.9129 6.48848 11.8875 6.45703C11.8624 6.42596 11.7923 6.33563 11.7306 6.2561C11.6869 6.1998 11.6472 6.14858 11.631 6.12815C11.0451 5.38901 10.4618 5 9.54545 5H4.63636C3.73262 5 3 5.73262 3 6.63636V18.0909C3 18.9946 3.73262 19.7273 4.63636 19.7273H19.3636C20.2674 19.7273 21 18.9946 21 18.0909V8.27273Z" /></svg>${item.label}`
-								}
-							]
+				}
+			);
+
+			// ANCHOR | Allow to open the file or a directory (replace the next sibling in normal mode).
+			itemElm.appendChild(
+				chess(
+					{ 
+						type: 'a',
+						attributes: { 
+							class: 'bwr__item__a non-editable',
+							title: lab.bt.open 
 						},
-						{ // Replace the precedent sibling in edit mode.
+						events: [
+							{
+								type: 'click',
+								function: () => {
+									if(item.type === 'file') {
+										accessFile(item.path);
+									}
+									else {
+										browseDirectory(item.path);
+									}
+								}
+							},
+						],
+						children : [
+							{
+								type: 'p',
+								attributes: {
+									class: 'bwr__item__title'
+								},
+								html: item.type === 'file' ? 
+									`<svg viewBox="3 3 18 18"><path d="m15.157 3h-8.5207c-0.90374 0-1.6364 0.73262-1.6364 1.6364v14.727c0 0.9038 0.73262 1.6364 1.6364 1.6364h11.455c0.9037 0 1.6364-0.7326 1.6364-1.6364v-11.793zm-0.3389 4.9091v-2.9338l2.9338 2.9338z" /></svg>${item.label}` :
+									`<svg viewBox="3 3 18 18"><path d="M21 8.27273C21 7.36899 20.2674 6.63636 19.3636 6.63636H12.0015C12.0343 6.63619 12.0239 6.6235 11.9519 6.53598C11.9342 6.51449 11.9129 6.48848 11.8875 6.45703C11.8624 6.42596 11.7923 6.33563 11.7306 6.2561C11.6869 6.1998 11.6472 6.14858 11.631 6.12815C11.0451 5.38901 10.4618 5 9.54545 5H4.63636C3.73262 5 3 5.73262 3 6.63636V18.0909C3 18.9946 3.73262 19.7273 4.63636 19.7273H19.3636C20.2674 19.7273 21 18.9946 21 18.0909V8.27273Z" /></svg>${item.label}`
+							}
+						]
+					}
+				)
+			);
+
+			// INPUT | Allow to rename the file or the directory (replace the precedent sibling in edit mode).
+			if(item.type !== 'parent') {
+				itemElm.appendChild(
+					chess(
+						{
 							type: 'input',
 							attributes: { 
 								class: 'editable',
@@ -272,10 +284,10 @@
 							],
 							text: item.label
 						}	
-					]
-				}
-			);
-			
+					)
+				);
+			}
+		
 			if(item.type === 'file' || item.type === 'subfolder') {
 			
 				itemElm.appendChild(
@@ -285,9 +297,9 @@
 							attributes: {
 								class: 'non-editable'
 							},
+							// BUTTON | Allow to download the file or the directory as a zip.
 							children: [
-
-								{ // Download elm.
+								{
 									type: 'button', 
 									attributes: { 
 										class: 'bwr__item__button',
@@ -301,8 +313,8 @@
 									],
 									html: '<svg class="bwr__item__button-svg" viewBox="-3 -3 30 30"><path d="M12 21l-8-9h6v-12h4v12h6l-8 9zm9-1v2h-18v-2h-2v4h22v-4h-2z"/></svg>'
 								},
-
-								{ // Remove elm.
+								// BUTTON | Allow to remove the file or the directory.
+								{
 									type: 'button',
 									attributes: {
 										class: 'bwr__item__button',
@@ -342,7 +354,6 @@
 									],
 									html: '<svg class="bwr__item__button-svg" viewBox="-3 -3 30 30"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>',
 								}
-
 							]
 						}
 					)
