379c716f73ed067071b12d3a88be5bb55fa5ff52
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-04-10 20:49:18 +0200

    Remove visible access to admin (always accessible by the owner only).

--- app/client/app-ui.js
+++ app/client/app-ui.js
@@ -8,7 +8,7 @@ const UI = {};
 UI.main = chess({ type: 'main' });
 UI.browser = chess({ type: 'section', attributes: { class: 'bwr' }});
 UI.browserNavTree = chess({ type: 'div', attributes: { class: 'bwr__nav__tree' }});
-UI.browserList = chess({ type: 'ul', attributes: { class: 'bwr__list' }});
+UI.browserList = chess({ type: 'ul', attributes: { class: 'bwr__list' }})
 
 UI.browserNav = chess(
 	{ 
@@ -177,20 +177,6 @@ UI.browserNav = chess(
 					}
 				],
 				html: '<svg viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10v-20zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"/></svg>'
-			},
-			{ // BUTTON | Open the admin page.
-				type: 'button',
-				attributes: {
-					class: 'bwr__nav__bt',
-					title: lab.bt.openAdmin
-				},
-				events: [
-					{
-						type: 'click',
-						function: () => window.location.replace('./pages/admin/')
-					}
-				],
-				html: '<svg viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.753 18.305c-.261-.586-.789-.991-1.871-1.241-2.293-.529-4.428-.993-3.393-2.945 3.145-5.942.833-9.119-2.489-9.119-3.388 0-5.644 3.299-2.489 9.119 1.066 1.964-1.148 2.427-3.393 2.945-1.084.25-1.608.658-1.867 1.246-1.405-1.723-2.251-3.919-2.251-6.31 0-5.514 4.486-10 10-10s10 4.486 10 10c0 2.389-.845 4.583-2.247 6.305z"/></svg>'
 			}
 		]
 	}
