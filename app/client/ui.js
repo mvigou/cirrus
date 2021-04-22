7c6eebf6955c5b1afdfcc18cc952b3e83ca6fac4
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-04-22 19:25:48 +0200

    Bye bye chess ;'(

--- app/client/ui.js
+++ app/client/ui.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// https://github.com/AwebsomeFr/chess
-const chess=t=>{let e=document.createElement(t.type);if(t.text?e.textContent=t.text:t.html&&(e.innerHTML=t.html),t.attributes)for(let n in t.attributes)e.setAttribute(n,t.attributes[n]);if(t.events)for(let n of t.events)e.addEventListener(n.type,n.function);if(t.children)for(let n of t.children)e.appendChild(chess(n));return e;};
-
 /* ### manage the appearance relative to the selected parent folder ### */
 
 	const toRecycleDirTheme = () => {
@@ -73,39 +70,19 @@ const chess=t=>{let e=document.createElement(t.type);if(t.text?e.textContent=t.t
 	};
 
 	const setPreviewImg = (sourcePath, tempPath) => {
-
-		ui.previewItem.appendChild(
-			chess(
-				{
-					type: 'img',
-					attributes: {
-						'class': 'preview__item__img',
-						'src': tempPath
-					}
-				}
-			)
-		);
-
+		let imgElm = document.createElement('img');
+		imgElm.classList.add('preview__item__img');
+		imgElm.src = tempPath;
+		ui.previewItem.appendChild(imgElm);
 		setPreview(sourcePath, tempPath);
-
 	};
 
 	const setPreviewPdf = (sourcePath, tempPath) => {
-
-		ui.previewItem.appendChild(
-			chess(
-				{
-					type: 'iframe',
-					attributes: {
-						'class': 'preview__item__iframe',
-						'src': tempPath
-					}
-				}
-			)
-		);
-
+		let iframeElm = document.createElement('iframe');
+		iframeElm.classList.add('preview__item__iframe');
+		iframeElm.src = tempPath;
+		ui.previewItem.appendChild(iframeElm);
 		setPreview(sourcePath, tempPath);
-
 	};
 
 /* ### hide / unhide the upload progression bar ### */
@@ -217,36 +194,20 @@ const chess=t=>{let e=document.createElement(t.type);if(t.text?e.textContent=t.t
 					
 			tree += '/';
 			tree += subDirs;
-			ui.tree.appendChild(
-				chess(
-					{
-						type: 'pre',
-						text: ' / '
-					}
-				)
-			);
 
-			ui.tree.appendChild(
-				chess(
-					{
-						type: 'a',
-						attributes: {
-							'href': tree
-						},
-						events: [
-							{
-								type: 'click',
-								function: e => {
-									browseDirectory('../../datas' + e.target.getAttribute('href'));
-									e.preventDefault();
-								} 
-							}
-						],
-						text: subDirs 
-					}
-				)
-			);
+			let preElm = document.createElement('pre');
+			preElm.textContent = ' / ';
+			ui.tree.appendChild(preElm);
 
+			let anchorElm = document.createElement('a');
+			anchorElm.href = tree;
+			anchorElm.textContent = subDirs;
+			anchorElm.onclick = e => {
+				browseDirectory('../../datas' + e.target.getAttribute('href'));
+				e.preventDefault();
+			};
+			ui.tree.appendChild(anchorElm);
+			
 		}
 		
 	};
@@ -269,154 +230,83 @@ const chess=t=>{let e=document.createElement(t.type);if(t.text?e.textContent=t.t
 					break;
 			}
 
-			let itemElm = chess(
-				{
-					type: 'li',
-					attributes: { 
-						class: 'list__item'
-					},
-				}
-			);
+			// Item main container.
+			
+				let itemElm;
+				itemElm = document.createElement('li');
+				itemElm.classList.add('list__item');
 
-			// Allow to open the file or a directory (replace the next sibling in normal mode).
-			itemElm.appendChild(
-				chess(
-					{ 
-						type: 'a',
-						attributes: { 
-							class: 'list__item__a non-editable',
-							title: lab.bt.openItem
-						},
-						events: [
-							{
-								type: 'click',
-								function: () => {
-									if(item.type === 'file') {
-										openFile(item.path);
-									}
-									else {
-										browseDirectory(item.path);
-									}
-								}
-							},
-						],
-						children : [
-							{
-								type: 'p',
-								attributes: {
-									class: 'list__item__title'
-								},
-								html: item.type === 'file' ? 
-									`<svg viewBox="3 3 18 18"><path d="m15.157 3h-8.5207c-0.90374 0-1.6364 0.73262-1.6364 1.6364v14.727c0 0.9038 0.73262 1.6364 1.6364 1.6364h11.455c0.9037 0 1.6364-0.7326 1.6364-1.6364v-11.793zm-0.3389 4.9091v-2.9338l2.9338 2.9338z" /></svg>${item.label}` :
-									`<svg viewBox="3 3 18 18"><path d="M21 8.27273C21 7.36899 20.2674 6.63636 19.3636 6.63636H12.0015C12.0343 6.63619 12.0239 6.6235 11.9519 6.53598C11.9342 6.51449 11.9129 6.48848 11.8875 6.45703C11.8624 6.42596 11.7923 6.33563 11.7306 6.2561C11.6869 6.1998 11.6472 6.14858 11.631 6.12815C11.0451 5.38901 10.4618 5 9.54545 5H4.63636C3.73262 5 3 5.73262 3 6.63636V18.0909C3 18.9946 3.73262 19.7273 4.63636 19.7273H19.3636C20.2674 19.7273 21 18.9946 21 18.0909V8.27273Z" /></svg>${item.label}`
-							}
-						]
-					}
-				)
-			);
+			// Allow to open the file or the directory (replace the next sibling in normal mode).
+		
+				let anchorItemElm = document.createElement('a');
+				anchorItemElm.classList.add('list__item__a', 'non-editable');
+				anchorItemElm.title = lab.bt.openItem;
+				anchorItemElm.onclick = () => item.type === 'file' ? openFile(item.path) : browseDirectory(item.path);
+				
+				let titleItemElm = document.createElement('p');
+				titleItemElm.classList.add('list__item__title');
+				titleItemElm.innerHTML = item.type === 'file' ? 
+					`<svg viewBox="3 3 18 18"><path d="m15.157 3h-8.5207c-0.90374 0-1.6364 0.73262-1.6364 1.6364v14.727c0 0.9038 0.73262 1.6364 1.6364 1.6364h11.455c0.9037 0 1.6364-0.7326 1.6364-1.6364v-11.793zm-0.3389 4.9091v-2.9338l2.9338 2.9338z" /></svg>${item.label}` :
+					`<svg viewBox="3 3 18 18"><path d="M21 8.27273C21 7.36899 20.2674 6.63636 19.3636 6.63636H12.0015C12.0343 6.63619 12.0239 6.6235 11.9519 6.53598C11.9342 6.51449 11.9129 6.48848 11.8875 6.45703C11.8624 6.42596 11.7923 6.33563 11.7306 6.2561C11.6869 6.1998 11.6472 6.14858 11.631 6.12815C11.0451 5.38901 10.4618 5 9.54545 5H4.63636C3.73262 5 3 5.73262 3 6.63636V18.0909C3 18.9946 3.73262 19.7273 4.63636 19.7273H19.3636C20.2674 19.7273 21 18.9946 21 18.0909V8.27273Z" /></svg>${item.label}`;	
+				
+				anchorItemElm.appendChild(titleItemElm);
+				itemElm.appendChild(anchorItemElm);
 
 			// Allow to rename the file or the directory (replace the precedent sibling in edit mode).
-			if(item.type !== 'parent') {
-				itemElm.appendChild(
-					chess(
-						{
-							type: 'input',
-							attributes: { 
-								class: 'editable',
-								value: item.label
-							},
-							events: [
-								{
-									type: 'keydown',
-									function: e => {
-										if(e.code === 'Enter') {
-											e.target.blur();
-										}
-									},
-								},
-								{
-									type: 'blur',
-									function: e => {
-										renameItem(item.label, e.target.value, e.target);
-									}		
-								}
-							],
-							text: item.label
-						}	
-					)
-				);
-			}
+			
+				if(item.type !== 'parent') {
+
+					let editItemInputElm = document.createElement('input');
+					editItemInputElm.classList.add('editable');
+					editItemInputElm.value = item.label;
+					editItemInputElm.textContent = item.label;
+					editItemInputElm.onkeydown = e => {
+						if(e.code === 'Enter') {
+							e.target.blur();
+						}
+					};
+					editItemInputElm.onblur = e => {
+						renameItem(item.label, e.target.value, e.target);
+					};
+					itemElm.appendChild(editItemInputElm);
+
+				}
 		
 			if(item.type === 'file' || item.type === 'subfolder') {
 			
 				// Allow to download the file or the directory as a zip.
-				itemElm.appendChild(
-					chess(
-						{
-							type: 'button', 
-							attributes: { 
-								class: 'non-editable',
-								title: lab.bt.downloadItem
-							},
-							events: [
-								{
-									type: 'click',
-									function: () => downloadUnpreviewedItem(item.path)
-								}
-							],
-							html: '<svg viewBox="-3 -3 30 30"><path d="M12 21l-8-9h6v-12h4v12h6l-8 9zm9-1v2h-18v-2h-2v4h22v-4h-2z"/></svg>'
-						}
-					)
-				);
+			
+					let downloadItemBtElm = document.createElement('button');
+					downloadItemBtElm.classList.add('non-editable');
+					downloadItemBtElm.title = lab.bt.downloadItem;
+					downloadItemBtElm.innerHTML = '<svg viewBox="-3 -3 30 30"><path d="M12 21l-8-9h6v-12h4v12h6l-8 9zm9-1v2h-18v-2h-2v4h22v-4h-2z"/></svg>';
+					downloadItemBtElm.onclick = () => {
+						downloadUnpreviewedItem(item.path)
+					};
+					itemElm.appendChild(downloadItemBtElm);
 
 				// Allow to remove the file or the directory.
-				itemElm.appendChild(
-					chess(
-						{
-							type: 'button',
-							attributes: {
-								class: 'publisher-ft non-editable',
-								title: lab.bt.removeItem
-							},
-							events: [
-								{
-									type: 'mousedown',
-									function: () => watchConfirmClick('start', item.path)
-								},
-								{
-									type: 'mouseup',
-									function: () => {
-										watchConfirmClick('end', item.path);
-										removeItem(item.path);
-									}
-								},
-								{
-									type: 'mouseleave',
-									function: () => cancelConfirm()
-								},
-								{
-									type: 'touchstart',
-									function: e => watchConfirmTouch('start', e)
-								},
-								{
-									type: 'touchend',
-									function: e => { 
-										watchConfirmTouch('end', e);
-										removeItem(item.path);
-									}
-								},
-								{
-									type: 'touchmove',
-									function: () => cancelConfirm()
-								}
-							],
-							html: '<svg viewBox="-3 -3 30 30"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>',
-						}
-					)
-				);
-
+				
+					let removeItemBtElm = document.createElement('button');
+					removeItemBtElm.classList.add('publisher-ft', 'non-editable');
+					removeItemBtElm.title = lab.bt.removeItem;
+					removeItemBtElm.innerHTML = '<svg viewBox="-3 -3 30 30"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>';
+					removeItemBtElm.onmousedown = () => watchConfirmClick('start', item.path);
+					removeItemBtElm.onmouseleave = () => cancelConfirm();
+					removeItemBtElm.ontouchstart = e => watchConfirmTouch('start', e);
+					removeItemBtElm.ontouchmove = () => cancelConfirm();
+					removeItemBtElm.onmouseup = () => {
+						watchConfirmClick('end', item.path);
+						removeItem(item.path);
+					};
+					removeItemBtElm.ontouchend = e => {
+						watchConfirmTouch('end', e);
+						removeItem(item.path);
+					};
+					itemElm.appendChild(removeItemBtElm);
+	
 			}
-			
+
 			ui.list.appendChild(itemElm);
 
 		}
