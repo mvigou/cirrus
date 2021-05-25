ffca465abb0748757aaf7b3d1402b55a318f0957
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-05-25 16:25:22 +0200

    Refactoring.

--- app/server/sign-up.php
+++ app/server/sign-up.php
@@ -1,29 +1,34 @@
 <?php if(
-	isset($_POST['user-name']) && 
-	isset($_POST['user-pass']) && 
-	isset($_POST['user-pass-conf']) &&
-	isset($_POST['role']) &&
-	isset($_POST['auth'])
+	isset(
+		$_POST['user-name'], 
+		$_POST['user-pass'], 
+		$_POST['user-pass-conf'], 
+		$_POST['role'], 
+		$_POST['auth']
+	)
 ){
 	if(
 		preg_match('/^[a-zA-Z0-9]{8,16}$/', $_POST['user-name']) &&
 		preg_match('/^[a-zA-Z0-9.?!\-_*+=\/|\\()[\]#$@%]{8,24}$/', $_POST['user-pass']) &&
-		$_POST['user-pass'] === $_POST['user-pass-conf']			
+		$_POST['user-pass'] === $_POST['user-pass-conf'] &&
+		strlen($_POST['auth']) === 128	
 	){
 		$env = json_decode(file_get_contents('../../datas/env.json'));
 		$authFilePath = null;
-		$userDirPath;
-		if($_POST['role'] === 'owner') {
-			$authFilePath = '../../datas/auth/sign-up-as-owner/' . $_POST['auth'];
-			$userDirPath = $env->ownersDir;
-		}
-		else if($_POST['role'] === 'publisher') {
-			$authFilePath = '../../datas/auth/sign-up-as-publisher/' . $_POST['auth'];
-			$userDirPath = $env->publishersDir;
-		}
-		else if($_POST['role'] === 'viewer') {
-			$authFilePath = '../../datas/auth/sign-up-as-viewer/' . $_POST['auth'];
-			$userDirPath = $env->viewersDir;
+		$userDirPath;		
+		switch($_POST['role']) {
+			case 'owner';
+				$authFilePath = $env->signUpOwnerAuthDir . '/' . $_POST['auth'];
+				$userDirPath = $env->ownersDir;
+				break;
+			case 'publisher';
+				$authFilePath = $env->signUpPublisherAuthDir . '/' . $_POST['auth'];
+				$userDirPath = $env->publishersDir;
+				break;
+			case 'viewer';
+				$authFilePath = $env->signUpViewerAuthDir . '/' . $_POST['auth'];
+				$userDirPath = $env->viewersDir;
+				break;	
 		}
 		if($authFilePath !== null){
 			if(is_file($authFilePath)) {
