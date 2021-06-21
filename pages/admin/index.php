eccb4c493a4814430d349bc7d976071789e52dbb
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-06-21 22:18:43 +0200

    Update description and order.

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -21,40 +21,24 @@ if(isAuthenticated() && hasOwnerRights()) { ?>
 					/>
 					<h1>cirrus | <span>administration</span></h1>
 				</header>
-				<!-- Toggle between light and dark theme. -->
 				<button 
 					onclick="switchTheme()"
 					title="Basculer entre thème clair / thème sombre">
 					<svg viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10v-20zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"/></svg>
 				</button>
 				<a href="../../">Retour</a>
-				<!-- Create, remove a public access. -->
 				<details ontoggle="managePublicAccess('browse')">
-					<summary>Accès public</summary>
+					<summary>Lien d'accès public</summary>
 					<ul id="public-access__list"></ul>
 					<button onclick="managePublicAccess('create')">
 						<svg viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
-						Activer l'accès
+						Activer le lien d'accès public
 					</button>
 					<button onclick="managePublicAccess('remove')">
 						<svg viewBox="-3 -3 30 30"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
-						Désactiver l'accès
+						Désactiver le lien d'accès public
 					</button>
 				</details>
-				<!-- list, create and remove publisher invitations -->
-				<details ontoggle="manageInvitations('browse', 'publisher')">
-					<summary>Invitations "éditeur"</summary>
-					<ul id="publisher__list"></ul>
-					<button onclick="manageInvitations('create', 'publisher')">
-						<svg viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
-						Générer un lien d'inscription
-					</button>
-					<button onclick="manageInvitations('remove', 'publisher')">
-						<svg viewBox="-3 -3 30 30"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
-						Supprimer les invitations
-					</button>
-				</details>
-				<!-- list, create and remove viewer invitations -->
 				<details ontoggle="manageInvitations('browse', 'viewer')">
 					<summary>Invitations "visualiseur"</summary>
 					<ul id="viewer__list"></ul>
@@ -67,12 +51,22 @@ if(isAuthenticated() && hasOwnerRights()) { ?>
 						Supprimer les invitations
 					</button>
 				</details>
-				<!-- list, change and remove accounts -->
+				<details ontoggle="manageInvitations('browse', 'publisher')">
+					<summary>Invitations "éditeur"</summary>
+					<ul id="publisher__list"></ul>
+					<button onclick="manageInvitations('create', 'publisher')">
+						<svg viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+						Générer un lien d'inscription
+					</button>
+					<button onclick="manageInvitations('remove', 'publisher')">
+						<svg viewBox="-3 -3 30 30"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
+						Supprimer les invitations
+					</button>
+				</details>
 				<details ontoggle="browseUsers()">
 					<summary>Gérer les utilisateurs</summary>
 					<ul id="users__list"></ul>
 				</details>
-				<!-- list and clear logs -->
 				<details ontoggle="manageLogs('browse')">
 					<summary>Journaux</summary>
 					<ul id="logs__list"></ul>
